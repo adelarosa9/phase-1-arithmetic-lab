52e2c4e272c71634bdec7a37a5525d1baca6918e
alejandro delarosa <a.delarosa9@yahoo.com>
2023-12-14 15:53:07 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 20;
+const num4 = 16;
+const mod = num3 % num4;
+
+const numberSet = [10, 5, 20, 15];
+const max = Math.max(...numberSet);
